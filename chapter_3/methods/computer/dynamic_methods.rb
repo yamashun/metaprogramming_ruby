a088dfd6e431fec0710be59d85d258c6a8d77afc
山田 俊輔 <yshunske@gmail.com>
2019-05-01 19:06:40 +0900

    さらに重複を排除

--- chapter_3/methods/computer/dynamic_methods.rb
+++ chapter_3/methods/computer/dynamic_methods.rb
@@ -2,6 +2,7 @@ class Computer
   def initialize(computer_id, data_source)
     @id = computer_id
     @data_source = data_source
+    data_source.methods.grep(/^get_(.*)_info$/) { Computer.define_component $1 }
   end
 
   def self.define_component(name)
@@ -13,8 +14,22 @@ class Computer
       result
     end
   end
-
-  define_component :mouse
-  define_component :cpu
-  define_component :keyboard
 end
+
+
+class MouseData
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+
+  def get_mouse_info(id)
+    @name
+  end
+  def get_mouse_price(id)
+    @price
+  end
+end
+
+c = Computer.new(1, MouseData.new('mouse', 100))
+puts c.mouse
