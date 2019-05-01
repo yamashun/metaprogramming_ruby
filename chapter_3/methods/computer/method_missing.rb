8596a6eb3749a11783b13f311658ec40318a9cb1
山田 俊輔 <yshunske@gmail.com>
2019-05-01 19:35:36 +0900

    respond_to_missing? ゴーストメソッドにも反応するようにするには、respond_to_missing?を使う

--- chapter_3/methods/computer/method_missing.rb
+++ chapter_3/methods/computer/method_missing.rb
@@ -12,5 +12,9 @@ class Computer
     return "* #{result}" if price >= 100
     result
   end
+
+  def respond_to_missing?(method, include_private = false)
+    @data_source.respond_to?("get_#{method}_info") || super
+  end
 end
 
